59a2f0d393b76b02f47e48c37434a683eaa98c82
mdrafe <37043268+mdrafe549@users.noreply.github.com>
2021-08-31 21:37:22 +0530

    Update Factorial.cs

--- Factorial.cs
+++ Factorial.cs
@@ -11,11 +11,16 @@ namespace factorialprogram
         public static void Main(string[] args)
         {
             int i, fact = 1, number;
+
+            //To take input from users
+
             Console.Write("enter number");
             number = int.Parse(Console.ReadLine());
             Console.Read();
+            //Loop for iteration of every number
             for(i=1;i<=number;i++)
             {
+             //Function for getting factors of each number
                 fact=fact*i;
             }
             Console.Write("factorial of" + number + "is"+fact);
